931dbaee9d4c66072853a9e39cf80fddb20cad71
yatharthnanda <yatharthnanda25@gmail.com>
2023-05-23 19:23:38 +0530

    initial commit

--- auto/src/Main.java
+++ auto/src/Main.java
@@ -1,14 +1,14 @@
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 
-import java.util.Scanner
+import java.util.Scanner;
 
 
     public class Main {
         public static void main(String[] args) {
             // Press Alt+Enter with your caret at the highlighted text to see how
             // IntelliJ IDEA suggests fixing it.
-            System.out.println("Helloworld");
+            System.out.println("Hello world");
 
 
         }
